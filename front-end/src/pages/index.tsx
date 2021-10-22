1dbaf854229ef7d29447e969e5d666304b8e578a
RalXYZ <RalXYZ@pm.me>
2021-10-22 21:41:43 +0800

    refactor: change gatsby js to tsx

---
+++ front-end/src/pages/index.tsx
@@ -0,0 +1,14 @@
+import * as React from 'react'
+
+const IndexPage = () => {
+  return (
+      <main>
+        <title>Home Page</title>
+        <h1>Welcome to my Gatsby site!</h1>
+        <p>I'm making this by following the Gatsby Tutorial.</p>
+        <button>Home</button>
+      </main>
+  )
+}
+
+export default IndexPage
