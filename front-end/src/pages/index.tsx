42d932c3da895729216634b62507fb4a57ec8457
RalXYZ <RalXYZ@pm.me>
2021-10-26 15:17:02 +0800

    feat: add landing page and footer

--- front-end/src/pages/index.tsx
+++ front-end/src/pages/index.tsx
@@ -1,17 +1,44 @@
-import * as React from 'react';
-import Navbar from '../components/navbar';
-import ArtworkCard from '../components/artworkCard';
+import * as React from "react";
+import Navbar from "../components/navbar";
+import Footer from "../components/footer";
+import ArtworkCard from "../components/artworkCard";
 
 const IndexPage = () => {
   return (
-      <main>
-        <Navbar/>
-        <title>Home Page</title>
-        <h1>Welcome to my Gatsby site!</h1>
-        <p>I'm making this by following the Gatsby Tutorial.</p>
-        <button className="btn btn-primary">IPFS Upload</button>
-      </main>
-  )
-}
+    <main>
+      <Navbar />
+      <div className="card">
+        <div
+          className="hero min-h-screen"
+          style={{
+            backgroundImage: `url("https://images.pexels.com/photos/158571/architecture-about-building-modern-158571.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940")`,
+          }}
+        >
+          <div className="hero-overlay bg-opacity-60"></div>
+          <div className="card glass lg:card-side text-neutral-content">
+            <figure className="p-6">
+              <img
+                src="https://picsum.photos/id/1005/300/200"
+                className="rounded-lg shadow-lg"
+              />
+            </figure>
+            <div className="max-w-md card-body">
+              <h2 className="card-title">Art, Persistent, Safe</h2>
+              <p>
+                Rerum reiciendis beatae tenetur excepturi aut pariatur est eos.
+                Sit sit necessitatibus veritatis sed molestiae voluptates
+                incidunt iure sapiente.
+              </p>
+              <div className="card-actions">
+                <button className="btn glass rounded-full">Get Started</button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <Footer />
+    </main>
+  );
+};
 
 export default IndexPage;
