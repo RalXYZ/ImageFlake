0a3be94ff55e5e75b4222aafe3c9f68e315e11b2
RalXYZ <RalXYZ@pm.me>
2021-10-26 21:59:10 +0800

    feat: update publish page

--- front-end/src/pages/publish.tsx
+++ front-end/src/pages/publish.tsx
@@ -14,13 +14,19 @@ export interface AlertInterface {
 
 class Publish extends Component<
   {},
-  { file: File; backgroundUrl: string; submitting: boolean; alert: AlertInterface }
+  {
+    file: File;
+    backgroundUrl: string;
+    submitting: boolean;
+    alert: AlertInterface;
+  }
 > {
   constructor(props) {
     super(props);
     this.state = {
       file: null,
-      backgroundUrl: "https://images.pexels.com/photos/158571/architecture-about-building-modern-158571.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+      backgroundUrl:
+        "https://images.pexels.com/photos/158571/architecture-about-building-modern-158571.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
       submitting: false,
       alert: {
         state: "error",
@@ -39,11 +45,11 @@ class Publish extends Component<
         file: e.target.files[0],
       });
       console.log(this.state.file);
-      let fileReader = new FileReader;
+      let fileReader = new FileReader();
       fileReader.onload = () => {
-        this.setState({backgroundUrl: fileReader.result as string});
-      }
-      fileReader.readAsDataURL(this.state.file)
+        this.setState({ backgroundUrl: fileReader.result as string });
+      };
+      fileReader.readAsDataURL(this.state.file);
     }
   }
 
@@ -97,82 +103,76 @@ class Publish extends Component<
   render() {
     return (
       <div>
-        <Navbar currentTab="publish" />
+        <Navbar currentTab="publish"/>
+        <div>
+          <div className="flex card lg:card-side bordered">
+            <figure className="flex-2 card">
+              <img src={this.state.backgroundUrl} />
+            </figure>
+            <div className="flex-1 max-w-lg my-8 mx-8">
+              <div className="form-control">
+                <form onSubmit={this.handleSubmit}>
+                  <label className="label">
+                    <span className="label-text">Name</span>
+                  </label>
+                  <input
+                    type="text"
+                    placeholder="name"
+                    className="input input-bordered"
+                  />
 
-        <div className="card">
-          <div
-            className="hero min-h-screen"
-            style={{
-              backgroundImage: `url(${this.state.backgroundUrl})`,
-            }}
-          >
-            <div className="hero-overlay bg-opacity-60"></div>
-            <div className="card glass lg:card-side text-neutral-content">
-              <div className="max-w-md card-body">
-                <div className="form-control">
-                  <form onSubmit={this.handleSubmit}>
-                    <label className="label">
-                      <span className="label-text">Name</span>
-                    </label>
-                    <input
-                      type="text"
-                      placeholder="name"
-                      className="input glass"
-                    />
+                  <label className="label">
+                    <span className="label-text">Description</span>
+                  </label>
+                  <input
+                    type="text"
+                    placeholder="description"
+                    className="input input-bordered"
+                  />
 
-                    <label className="label">
-                      <span className="label-text">Description</span>
-                    </label>
-                    <input
-                      type="text"
-                      placeholder="description"
-                      className="input glass"
-                    />
-
-                    <label className="label">
-                      <span className="label-text">Artwork</span>
-                    </label>
-                    <label
-                      className={`btn btn-outline ${
-                        this.state.file === null ? "" : "btn-accent"
-                      }`}
+                  <label className="label">
+                    <span className="label-text">Artwork</span>
+                  </label>
+                  <label
+                    className={`btn btn-outline ${
+                      this.state.file === null ? "" : "btn-accent"
+                    }`}
+                  >
+                    <span>
+                      {this.state.file === null
+                        ? "Select a file"
+                        : this.state.file.name}
+                    </span>
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      className="inline-block w-6 h-6 ml-2 stroke-current"
                     >
-                      <span>
-                        {this.state.file === null
-                          ? "Select a file"
-                          : this.state.file.name}
-                      </span>
-                      <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        fill="none"
-                        viewBox="0 0 24 24"
-                        className="inline-block w-6 h-6 ml-2 stroke-current"
-                      >
-                        <path
-                          strokeLinecap="round"
-                          strokeLinejoin="round"
-                          strokeWidth="2"
-                          d="M9 5l7 7-7 7"
-                        ></path>
-                      </svg>
-                      <input
-                        type="file"
-                        name="myImage"
-                        onChange={this.onImageChange}
-                        className="hidden"
-                      />
-                    </label>
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        strokeWidth="2"
+                        d="M9 5l7 7-7 7"
+                      ></path>
+                    </svg>
+                    <input
+                      type="file"
+                      name="myImage"
+                      onChange={this.onImageChange}
+                      className="hidden"
+                    />
+                  </label>
 
-                    <button
-                      className={`btn btn-primary btn-lg block my-4 ${
-                        this.state.submitting ? "loading" : ""
-                      }`}
-                      type="submit"
-                    >
-                      Submit Artwork
-                    </button>
-                  </form>
-                </div>
+                  <button
+                    className={`btn btn-primary btn-lg block my-4 ${
+                      this.state.submitting ? "loading" : ""
+                    }`}
+                    type="submit"
+                  >
+                    Submit Artwork
+                  </button>
+                </form>
               </div>
             </div>
           </div>
